578b32d6a03b49f4f57f4616d598d09c48b5d8f7
Nurul Azim Sakib <sakibruns@gmail.com>
2024-12-23 05:57:44 +0600

    Gradient more upgraded

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,16 @@ export default {
         radial-gradient(circle at bottom right, rgba(66, 46, 113, 0.6) 0%, rgba(66, 46, 113, 0.8) 20%, rgba(96, 67, 163, 1) 70%, rgba(96, 67, 163, 1) 100%)
       `,
         "prime-hosting-gradient": `
-          radial-gradient(circle at bottom left, rgba(66, 46, 113, 0.6) 0%,  rgba(2, 0, 36, 1) 90%, rgba(2, 0, 36, 1) 100%)`,
+          radial-gradient(circle at bottom left, rgba(66, 46, 113, 0.6) 0%,  rgba(2, 0, 36, 1) 90%, rgba(2, 0, 36, 1) 100%)`
+        ,
+        "New": `
+        linear-gradient(90deg, rgba(96,67,163,1) 50.34%, rgba(2,0,36,1) 76%);
+
+          `,
+        "new":
+          `
+          linear-gradient(90deg, rgba(96,67,163,1) 33.34%, rgba(155,160,181,1) 47%, rgba(255,255,255,1) 100%);
+          `
       },
     },
   },
